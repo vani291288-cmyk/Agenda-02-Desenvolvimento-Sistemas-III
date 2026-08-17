ab1178d3e73029bb1048253e353fd23022d2d0bd
vani291288-cmyk <vani291288@gmail.com>
2026-08-17 01:47:42 -0300

    Criação do SQLiteDatabaseHelper

---
+++ Models/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,42 @@
+using MauiAppMinhasCompras.Models;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Helpers;
+
+public class SQLiteDatabaseHelper
+{
+    private readonly SQLiteAsyncConnection _conn;
+
+    public SQLiteDatabaseHelper(string path)
+    {
+        _conn = new SQLiteAsyncConnection(path);
+        _conn.CreateTableAsync<Produto>().Wait();
+    }
+
+    public Task<int> Insert(Produto p)
+    {
+        return _conn.InsertAsync(p);
+    }
+
+    public Task<int> Update(Produto p)
+    {
+        return _conn.UpdateAsync(p);
+    }
+
+    public Task<int> Delete(int id)
+    {
+        return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+    }
+
+    public Task<List<Produto>> GetAll()
+    {
+        return _conn.Table<Produto>().ToListAsync();
+    }
+
+    public Task<List<Produto>> Search(string q)
+    {
+        return _conn.Table<Produto>()
+                    .Where(p => p.Descricao.Contains(q))
+                    .ToListAsync();
+    }
+}
