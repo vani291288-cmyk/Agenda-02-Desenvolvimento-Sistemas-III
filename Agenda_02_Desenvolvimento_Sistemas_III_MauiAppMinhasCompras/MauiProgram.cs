eac2d252cdd361d95678bdaddacf43d5ad0f71ce
vani291288-cmyk <vani291288@gmail.com>
2026-09-07 08:08:09 -0300

    Corrige configuração de logging no MAUI 10

--- Agenda_02_Desenvolvimento_Sistemas_III_MauiAppMinhasCompras/MauiProgram.cs
+++ Agenda_02_Desenvolvimento_Sistemas_III_MauiAppMinhasCompras/MauiProgram.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.Logging;
-
 namespace MauiAppMinhasCompras;
 
 public static class MauiProgram
@@ -15,10 +13,6 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
-
-#if DEBUG
-        builder.Logging.AddDebug();
-#endif
 
         return builder.Build();
     }
