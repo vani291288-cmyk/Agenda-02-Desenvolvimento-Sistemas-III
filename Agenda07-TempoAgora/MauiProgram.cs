2ac84e9d948014e13ab88cbbd130a88cb2d96641
vani291288-cmyk <vani291288@gmail.com>
2026-09-20 14:09:49 -0300

    Agenda 07 - implementar Tempo Agora corrigido

---
+++ Agenda07-TempoAgora/MauiProgram.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.Logging;
+
+namespace MauiAppTempoAgora;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+            .UseMauiApp<App>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
